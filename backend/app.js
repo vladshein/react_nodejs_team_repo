f976e636e418048f0d35201fb3fe9527deace88d
vladshein <vlshein1@gmail.com>
2025-12-10 10:09:02 +0200

    Added all data to db, commented out sync function

--- backend/app.js
+++ backend/app.js
@@ -1,31 +1,31 @@
-import express from "express";
-import morgan from "morgan";
-import cors from "cors";
-import "dotenv/config";
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import 'dotenv/config';
 
-import authRouter from "./routes/authRouter.js";
-import userRouter from "./routes/userRouter.js";
-import commonRouter from "./routes/commonRouter.js";
-import recipesRouter from "./routes/recipesRouter.js";
-import notFoundHandler from "./middlewares/notFoundHandler.js";
-import errorHandler from "./middlewares/errorHandler.js";
-import connectDatabase from "./db/connectDatabase.js";
+import authRouter from './routes/authRouter.js';
+import userRouter from './routes/userRouter.js';
+import commonRouter from './routes/commonRouter.js';
+import recipesRouter from './routes/recipesRouter.js';
+import notFoundHandler from './middlewares/notFoundHandler.js';
+import errorHandler from './middlewares/errorHandler.js';
+import connectDatabase from './db/connectDatabase.js';
 
-import { syncDatabase } from "./db/models/index.js";
+// import { syncDatabase } from './db/models/index.js';
 
 const app = express();
 
-syncDatabase();
+// syncDatabase();
 
-app.use(morgan("tiny"));
+app.use(morgan('tiny'));
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-app.use("/api/auth", authRouter);
-app.use("/api/users", userRouter);
-app.use("/api", commonRouter);
-app.use("/api/recipes", recipesRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/users', userRouter);
+app.use('/api', commonRouter);
+app.use('/api/recipes', recipesRouter);
 
 app.use(notFoundHandler);
 
@@ -36,5 +36,5 @@ await connectDatabase();
 const port = Number(process.env.PORT) | 3000;
 
 app.listen(port, () => {
-    console.log("Server is running. Use our API on port: 3000");
+  console.log('Server is running. Use our API on port: 3000');
 });
