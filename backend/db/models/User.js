a0f04fd64b1c8f8c8744763a33669494d186155b
vladshein <vlshein1@gmail.com>
2025-12-10 15:37:43 +0200

    added bson for ids generation, updated current router

--- backend/db/models/User.js
+++ backend/db/models/User.js
@@ -1,42 +1,42 @@
-import { DataTypes } from "sequelize";
-import sequelize from "../sequelize.js";
-import { emailRegExp } from "../../constants/authConstants.js";
+import { DataTypes } from 'sequelize';
+import sequelize from '../sequelize.js';
+import { emailRegExp } from '../../constants/authConstants.js';
 
-const User = sequelize.define("user", {
-    id: {
-        type: DataTypes.TEXT,
-        primaryKey: true,
+const User = sequelize.define('user', {
+  id: {
+    type: DataTypes.TEXT,
+    primaryKey: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: {
+      args: true,
+      msg: 'Email in use',
     },
-    name: {
-        type: DataTypes.STRING,
-        allowNull: false,
+    validate: {
+      is: emailRegExp,
     },
-    email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: {
-            args: true,
-            msg: "Email in use",
-        },
-        validate: {
-            is: emailRegExp,
-        },
-    },
-    password: {
-        type: DataTypes.STRING,
-        defaultValue: null,
-        allowNull: true,
-    },
-    avatar: {
-        type: DataTypes.STRING,
-        defaultValue: null,
-        allowNull: true,
-    },
-    token: {
-        type: DataTypes.STRING,
-        defaultValue: null,
-        allowNull: true,
-    },
+  },
+  password: {
+    type: DataTypes.STRING,
+    defaultValue: null,
+    allowNull: true,
+  },
+  avatar: {
+    type: DataTypes.STRING,
+    defaultValue: null,
+    allowNull: true,
+  },
+  token: {
+    type: DataTypes.STRING,
+    defaultValue: null,
+    allowNull: true,
+  },
 });
 
 export default User;
