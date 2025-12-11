15ed239301bcf56a80215ea405e69488db9d61d0
i3oi3ka <i3oi3ka@ukr.net>
2025-12-11 22:45:46 +0200

    Add getUserById endpoint and service for retrieving user information by ID

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import upload from '../middlewares/upload.js';
 import authenticate from '../middlewares/authenticate.js';
-import { getCurrentUser } from './../controllers/userControllers.js';
+import { getCurrentUser, getUserById } from './../controllers/userControllers.js';
 import { updateAvatarController, getFollowersController } from '../controllers/authControllers.js';
 
 const userRouter = express.Router();
 userRouter.get('/current', authenticate, getCurrentUser);
+userRouter.get('/:userId', authenticate, getUserById);
 userRouter.patch('/avatars', authenticate, upload.single('avatar'), updateAvatarController);
 userRouter.get('/followers', authenticate, getFollowersController);
 
