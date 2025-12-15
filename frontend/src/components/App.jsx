edf4b665e6a579a92219d2e2e7adf54339492e73
Borys <boris02374@gmail.com>
2025-12-15 23:36:11 +0200

    feat: add UserPage navigation with temporary mock_ data

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,113 +1,66 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
 import { lazy, Suspense } from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
 
 const HomePage = lazy(() => import('../pages/HomePage/HomePage'));
-const UserPage = lazy(() => import('../pages/UserPage/UserPage'));
 const RecipePage = lazy(() => import('../pages/RecipePage/RecipePage'));
 const AddRecipePage = lazy(() => import('../pages/AddRecipePage/AddRecipePage'));
+const UserPage = lazy(() => import('../pages/UserPage/UserPage'));
 const NotFoundPage = lazy(() => import('../pages/NotFoundPage/NotFoundPage'));
 
-// USER PROFILE SUB-COMPONENTS (SCAFFOLDING)
-// Ці компоненти зараз використовують мок-дані (hardcoded data) для візуалізації
-// структури вкладених роутів (Nested Routes).
-// TODO: Підключити реальну логіку Redux та замінити заглушки на робочий код.
+// --- USER PROFILE SUB-COMPONENTS ---
+// Ці компоненти зараз використовують мок-дані для візуалізації структури.
+// TODO: Підключити реальну логіку Redux та замінити заглушки.
 const UserRecipes = lazy(() => import('../components/SharedLayoutComponents/UserRecipes/UserRecipes'));
 const UserFavorites = lazy(() => import('../components/SharedLayoutComponents/UserFavorites/UserFavorites'));
 const UserFollowers = lazy(() => import('../components/SharedLayoutComponents/UserFollowers/UserFollowers'));
 const UserFollowing = lazy(() => import('../components/SharedLayoutComponents/UserFollowing/UserFollowing'));
 
+// import style from './App.module.css';
+import Categories from './HomePageComponents/Categories/Categories';
+import RestrictedRoute from '../guards/RestrictedRoute/RestrictedRoute';
 import PrivateRoute from '../guards/PrivateRoute/PrivateRoute';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectIsRefreshing } from '../redux/auth/selectors';
+import { useEffect } from 'react';
+import { refreshUser } from '../redux/auth/actions';
 
 const App = () => {
-  return (
+  const isRefreshing = useSelector(selectIsRefreshing);
+  console.log(isRefreshing);
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [dispatch]);
+
+  return isRefreshing ? (
+    <div>Refreshing user...</div>
+  ) : (
     <Suspense fallback={<div>Loading...</div>}>
+      {/* <div className={style.container}> */}
       <Routes>
         <Route path="/" element={<HomePage />} />
-
-        <Route path="/recipe/:id" element={<RecipePage />} />
+        
         <Route 
           path="/recipe/add" 
           element={<PrivateRoute component={<AddRecipePage />} />} 
         />
+        
+        <Route path="/recipe/:id" element={<RecipePage />} />
 
-        {/* /user/:id */}
         <Route path="/user/:id" element={<UserPage />}>
-
-          <Route index element={<Navigate to="recipes" replace />} />
-          
-          {/* /user/:id/recipes */}
-          <Route path="recipes" element={<UserRecipes />} />
-          
-          {/* /user/:id/favorites */}
-          <Route path="favorites" element={<UserFavorites />} />
-          
-          {/* /user/:id/followers */}
-          <Route path="followers" element={<UserFollowers />} />
-          
-          {/* /user/:id/following */}
-          <Route path="following" element={<UserFollowing />} />
-          
+           <Route index element={<Navigate to="recipes" replace />} />
+           <Route path="recipes" element={<UserRecipes />} />
+           <Route path="favorites" element={<UserFavorites />} />
+           <Route path="followers" element={<UserFollowers />} />
+           <Route path="following" element={<UserFollowing />} />
         </Route>
 
-        {/* 404 */}
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
+      {/* </div> */}
     </Suspense>
   );
 };
 
 export default App;
-
-// import { lazy, Suspense } from 'react';
-// import { Route, Routes, Navigate } from 'react-router-dom';
-
-// const HomePage = lazy(() => import('../pages/HomePage/HomePage'));
-// const RecipePage = lazy(() => import('../pages/RecipePage/RecipePage'));
-// const AddRecipePage = lazy(() => import('../pages/AddRecipePage/AddRecipePage'));
-// const UserPage = lazy(() => import('../pages/UserPage/UserPage'));
-// const NotFoundPage = lazy(() => import('../pages/NotFoundPage/NotFoundPage'));
-
-// import style from './App.module.css';
-// import Hero from './HomePageComponents/Hero/Hero';
-// import Categories from './HomePageComponents/Categories/Categories';
-// import RestrictedRoute from '../guards/RestrictedRoute/RestrictedRoute';
-// import PrivateRoute from '../guards/PrivateRoute/PrivateRoute';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { selectIsRefreshing } from '../redux/auth/selectors';
-// import { useEffect } from 'react';
-// import { refreshUser } from '../redux/auth/actions';
-
-// const App = () => {
-//   const isRefreshing = useSelector(selectIsRefreshing);
-//   console.log(isRefreshing);
-
-//   const dispatch = useDispatch();
-//   useEffect(() => {
-//     dispatch(refreshUser());
-//   }, [dispatch]);
-
-//   return isRefreshing ? (
-//     <div>Refreshing user...</div>
-//   ) : (
-//     <Suspense fallback={<div>Loading...</div>}>
-//       <div className={style.container}>
-//         <Routes>
-//           <Route path="/" element={<HomePage />}>
-//             <Route index element={<Navigate to="categories" />} />
-//             <Route path="categories" element={<Categories />}></Route>
-//             {/* <Route path="categories/:id" element={<Categories />}></Route> */}
-//           </Route>
-//           <Route index element={<HomePage />} />
-//           <Route
-//             path="/recipe/add"
-//             element={<PrivateRoute component={<AddRecipePage />} />}></Route>
-//           <Route path="/recipe/:id" element={<RecipePage />}></Route>
-//           <Route path="/user/:id" element={<UserPage />}></Route>
-//           <Route path="*" element={<NotFoundPage />}></Route>
-//         </Routes>
-//       </div>
-//     </Suspense>
-//   );
-// };
-
-// export default App;
