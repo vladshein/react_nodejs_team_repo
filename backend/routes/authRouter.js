440e97de1bb0368dff3245db0bda60074cd33324
i3oi3ka <i3oi3ka@ukr.net>
2025-12-15 21:23:00 +0200

    fix: resolve merge conflict in authRouter.js

--- backend/routes/authRouter.js
+++ backend/routes/authRouter.js
@@ -18,11 +18,7 @@ authRouter.post('/register', validateBody(registerSchema), registerController);
 authRouter.post('/login', validateBody(loginSchema), loginController);
 authRouter.get('/current', authenticate, getCurrentController);
 authRouter.post('/logout', authenticate, logoutController);
-<<<<<<< HEAD
 
-authRouter.get('/current', authenticate, getCurrentController);
-=======
->>>>>>> main
 // authRouter.patch("/avatars", authenticate, upload.single("avatar"), updateAvatarController);
 
 export default authRouter;
