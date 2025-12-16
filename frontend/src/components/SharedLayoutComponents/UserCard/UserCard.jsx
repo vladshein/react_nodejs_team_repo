5b782f64ae63f1dc0d54b97367d585899a25da2c
Borys <boris02374@gmail.com>
2025-12-17 00:44:27 +0200

    saved: user-card.css2

--- frontend/src/components/SharedLayoutComponents/UserCard/UserCard.jsx
+++ frontend/src/components/SharedLayoutComponents/UserCard/UserCard.jsx
@@ -1,5 +1,5 @@
 import styles from './UserCard.module.css';
-import sprite from '../../../../public/icons.svg'
+import sprite from '../../../../public/icons.svg';
 import defaultAvatar from '../../../../public/cat_avatar.png';
 
 const UserCard = ({ user }) => {
@@ -8,10 +8,10 @@ const UserCard = ({ user }) => {
   return (
     <li className={styles.card}>
       <div className={styles.avatarWrapper}>
-        <img 
-          src={avatar || defaultAvatar} 
-        alt={name} 
-        className={styles.avatar}
+        <img
+          src={avatar || defaultAvatar}
+          alt={name}
+          className={styles.avatar}
           width="60"
           height="60"
           loading="lazy"
@@ -21,23 +21,26 @@ const UserCard = ({ user }) => {
       <div className={styles.content}>
         <div className={styles.header}>
           <h3 className={styles.name}>{name}</h3>
-          
-          <a href={`/user/${id}`} className={styles.arrowLink} aria-label={`Go to ${name}'s profile`}>
-             <svg className={styles.iconArrow} width="18" height="18">
-                <use href={`${sprite}#icon-arrow-up-right`} />
-             </svg>
-          </a>
+          <p className={styles.recipesInfo}>Own recipes: {recipesCount}</p>
+
+          <button
+            className={isFollowing ? styles.unfollowBtn : styles.followBtn}
+            type="button"
+            aria-label={isFollowing ? `Unfollow ${name}` : `fOLLOW ${name}`}>
+            {isFollowing ? 'UNFOLLOW' : 'FOLLOW'}
+          </button>
         </div>
 
-        <p className={styles.recipesInfo}>Own recipes: {recipesCount}</p>
-
-        <button 
-          className={isFollowing ? styles.unfollowBtn : styles.followBtn}
-          type="button"
-          aria-label={isFollowing ? `Unfollow ${name}` : `Follow ${name}`}
-        >
-          {isFollowing ? 'UNFOLLOW' : 'FOLLOW'}
-        </button>
+        <div className={styles.content}>
+          <a
+            href={`/user/${id}`}
+            className={styles.arrowLink}
+            aria-label={`Go to ${name}'s profile`}>
+            <svg className={styles.iconArrow} width="18" height="18">
+              <use href={`${sprite}#icon-arrow-up-right`} />
+            </svg>
+          </a>
+        </div>
       </div>
     </li>
   );
