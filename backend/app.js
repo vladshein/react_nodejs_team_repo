858041e20b13bc8aadef2a8d8026fc9daf288df6
Pavlo <pavlo.rohozhyn@gmail.com>
2025-12-16 15:27:51 +0100

    remove bit-operation

--- backend/app.js
+++ backend/app.js
@@ -40,7 +40,7 @@ app.use(errorHandler);
 
 await connectDatabase();
 
-const port = Number(process.env.PORT) | 3000;
+const port = Number(process.env.PORT) || 3000;
 
 app.listen(port, () => {
   console.log(`Server is running. Use our API on port: ${port}`);
