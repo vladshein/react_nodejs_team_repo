d59987190b9eead992e3dd5350da86fbaee81d32
Borys <boris02374@gmail.com>
2025-12-17 11:50:33 +0200

    saved: user-card.css4

--- frontend/src/components/SharedLayoutComponents/UserCard/UserCard.jsx
+++ frontend/src/components/SharedLayoutComponents/UserCard/UserCard.jsx
@@ -18,9 +18,7 @@ const UserCard = ({ user }) => {
         />
       </div>
 
-      <div className={styles.inner}>
-        {' '}
-        {/* card__inner */}
+      <div className={styles.content}>
         <div className={styles.header}>
           <h3 className={styles.name}>{name}</h3>
           <p className={styles.recipesInfo}>Own recipes: {recipesCount}</p>
@@ -28,11 +26,12 @@ const UserCard = ({ user }) => {
           <button
             className={isFollowing ? styles.unfollowBtn : styles.followBtn}
             type="button"
-            aria-label={isFollowing ? `Unfollow ${name}` : `Follow ${name}`}>
+            aria-label={isFollowing ? `Unfollow ${name}` : `fOLLOW ${name}`}>
             {isFollowing ? 'UNFOLLOW' : 'FOLLOW'}
           </button>
         </div>
-        <div className={styles.actions}>
+
+        <div className={styles.content}>
           <a
             href={`/user/${id}`}
             className={styles.arrowLink}
@@ -48,45 +47,3 @@ const UserCard = ({ user }) => {
 };
 
 export default UserCard;
-
-//     <li className={styles.card}>
-//       <div className={styles.avatarWrapper}>
-//         <img
-//           src={avatar || defaultAvatar}
-//           alt={name}
-//           className={styles.avatar}
-//           width="60"
-//           height="60"
-//           loading="lazy"
-//         />
-//       </div>
-
-//       <div className={styles.content}>
-//         <div className={styles.header}>
-//           <h3 className={styles.name}>{name}</h3>
-//           <p className={styles.recipesInfo}>Own recipes: {recipesCount}</p>
-
-//           <button
-//             className={isFollowing ? styles.unfollowBtn : styles.followBtn}
-//             type="button"
-//             aria-label={isFollowing ? `Unfollow ${name}` : `fOLLOW ${name}`}>
-//             {isFollowing ? 'UNFOLLOW' : 'FOLLOW'}
-//           </button>
-//         </div>
-
-//         <div className={styles.content}>
-//           <a
-//             href={`/user/${id}`}
-//             className={styles.arrowLink}
-//             aria-label={`Go to ${name}'s profile`}>
-//             <svg className={styles.iconArrow} width="18" height="18">
-//               <use href="/icons.svg#icon-arrow-up-right" />
-//             </svg>
-//           </a>
-//         </div>
-//       </div>
-//     </li>
-//   );
-// };
-
-// export default UserCard;
