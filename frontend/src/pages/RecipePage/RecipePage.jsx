b6ac29c771e3676784665f7adf7f9e6bdf6ebb02
Pug10rd <vandal12313@gmail.com>
2025-12-18 21:10:15 +0100

    init commit

--- frontend/src/pages/RecipePage/RecipePage.jsx
+++ frontend/src/pages/RecipePage/RecipePage.jsx
@@ -1,38 +1,57 @@
-import { Outlet } from "react-router-dom";
-import styles from "./RecipePage.module.css";
-import Location from "../../components/Location/Location";
-import Filters from "../../components/Filters/Filters";
-import TruckList from "../../components/TruckList/TruckList";
-
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchTrucksOp } from "../../redux/trucksOps";
-import { selectError, selectLoading } from "../../redux/trucksSlice";
+import styles from './RecipePage.module.css';
+import RecipeInfo from '../../components/RecipePageComponents/RecipeInfo/RecipeInfo';
+import PopularRecipes from '../../components/RecipePageComponents/PopularRecipes/PopularRecipes';
+import RecipeMainInfo from '../../components/RecipePageComponents/RecipeMainInfo/RecipeMainInfo';
+import RecipePreparation from '../../components/RecipePageComponents/RecipePreparation/RecipePreparation';
+import RecipeIngredients from '../../components/RecipePageComponents/RecipeIngredients/RecipeIngredients';
+import Header from '../../components/SharedLayoutComponents/Header/Header';
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 const RecipePage = () => {
-  const dispatch = useDispatch();
-  const isLoading = useSelector(selectLoading);
-  const error = useSelector(selectError);
+  const { id } = useParams(); // recipe id from URL
+  const [recipe, setRecipe] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    dispatch(fetchTrucksOp());
-  }, [dispatch]);
+    const fetchRecipe = async () => {
+      try {
+        const res = await fetch(`http://localhost:3000/api/recipes/${id}`);
+        if (!res.ok) throw new Error('Failed to load recipe');
+
+        const data = await res.json();
+        setRecipe(data);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchRecipe();
+  }, [id]);
+
+  if (loading) return <div>Loading...</div>;
+  if (!recipe) return <div>Recipe not found</div>;
+
   return (
-    <div className={styles.catalogContainer}>
-      <div>
-        <Location />
-        <Filters />
+    <>
+      <Header />
+      <div className={styles.catalogContainer}>
+        <RecipeInfo>
+          <div className="columnContainerLeft">
+            {' '}
+            {recipe && <img src={recipe.thumb} className={styles.recipeImage} />}
+          </div>
+          <div className="columnContainerRight">
+            <RecipeMainInfo recipe={recipe} />
+            <RecipeIngredients ingredients={recipe.ingredients} />
+            <RecipePreparation instructions={recipe.instructions} />
+          </div>
+        </RecipeInfo>
+        <PopularRecipes />
       </div>
-      <div>
-        {isLoading ? (
-          <p>Loading...</p>
-        ) : error ? (
-          <p>Error: {error}</p>
-        ) : (
-          <TruckList />
-        )}
-      </div>
-    </div>
+    </>
   );
 };
 
