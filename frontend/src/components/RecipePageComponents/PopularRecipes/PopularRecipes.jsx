adf60a5674230f126b8b0aa8134825772d281c53
Pug10rd <vandal12313@gmail.com>
2025-12-21 20:43:06 +0100

    added hook for favorites, changed RecipeCard, implemented unified RecipeCard in PopularRecipes

--- frontend/src/components/RecipePageComponents/PopularRecipes/PopularRecipes.jsx
+++ frontend/src/components/RecipePageComponents/PopularRecipes/PopularRecipes.jsx
@@ -1,6 +1,7 @@
+import RecipeCard from '../RecipeCard/RecipeCard';
 import styles from './PopularRecipes.module.css';
 
-const PopularRecipes = ({ popular }) => {
+const PopularRecipes = ({ popular, isFavorite }) => {
   return (
     <section className={styles.popular}>
       <h2 className={styles.title}>Popular recipes</h2>
@@ -8,34 +9,7 @@ const PopularRecipes = ({ popular }) => {
       <div className={styles.cards}>
         {popular
           ? popular.map((recipe) => {
-              return (
-                <article key={recipe.id} className={styles.card}>
-                  <img src={recipe.thumb} alt={recipe.title} className={styles.cardImage} />
-
-                  <div className={styles.cardContent}>
-                    <div className={styles.cardText}>
-                      <h3 className={styles.cardTitle}>{recipe.title}</h3>
-                      <p className={styles.cardDescription}>{recipe.description}</p>
-                    </div>
-
-                    <div className={styles.cardFooter}>
-                      <div className={styles.user}>
-                        <img
-                          src={recipe.owner.avatar}
-                          alt={recipe.owner.name}
-                          className={styles.userAvatar}
-                        />
-                        <span className={styles.userName}>{recipe.owner.name}</span>
-                      </div>
-
-                      <div className={styles.icons}>
-                        <button className={styles.iconButton}>♥</button>
-                        <button className={styles.iconButton}>↗</button>
-                      </div>
-                    </div>
-                  </div>
-                </article>
-              );
+              return <RecipeCard recipe={recipe} isFavorite={isFavorite} />;
             })
           : null}
       </div>
