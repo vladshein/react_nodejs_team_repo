d9608e7dddc2bcee6db178e6da76b2269043ba75
Borys <boris02374@gmail.com>
2025-12-22 14:22:02 +0200

    added margin contWrapper

--- frontend/src/pages/UserPage/UserPage.jsx
+++ frontend/src/pages/UserPage/UserPage.jsx
@@ -45,22 +45,24 @@ const UserPage = () => {
   ) : (
     user && (
       <div className={styles.container}>
-        <Breadcrumbs paths={breadcrumbs} />
+        <div className={styles.containerWrapper}>
+          <Breadcrumbs paths={breadcrumbs} />
 
-        <div className={styles.pageWrapper}>
-          <h2 className={styles.title}>profile</h2>
-          <p className={styles.description}>
-            Reveal your culinary art, share your favorite recipe and create gastronomic masterpieces
-            with us.
-          </p>
-          <div className={styles.outletContainer}>
-            <aside className={styles.sidebar}>
-              <UserInfo user={user} />
-            </aside>
-            <main className={styles.content}>
-              <TabsList />
-              <Outlet />
-            </main>
+          <div className={styles.pageWrapper}>
+            <h2 className={styles.title}>profile</h2>
+            <p className={styles.description}>
+              Reveal your culinary art, share your favorite recipe and create gastronomic
+              masterpieces with us.
+            </p>
+            <div className={styles.outletContainer}>
+              <aside className={styles.sidebar}>
+                <UserInfo user={user} />
+              </aside>
+              <main className={styles.content}>
+                <TabsList />
+                <Outlet />
+              </main>
+            </div>
           </div>
         </div>
       </div>
